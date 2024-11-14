c69f44d0e3afe45119967760d3a954562059e175
Andrés <101900529+Wokus@users.noreply.github.com>
2024-11-14 19:34:30 -0300

    parte web del registro pronto(falta local)

---
+++ src/main/java/com/mycompany/espotifyweb/regiServlet.java
@@ -0,0 +1,137 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.mycompany.espotifyweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.Collection;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import logica.controladores.ControladorAlbum;
+import logica.controladores.ControladorCliente;
+import logica.controladores.ControladorSuscripcion;
+import logica.controladores.ControladorTema;
+import logica.dt.DataAlbum;
+import logica.dt.DataCliente;
+import logica.dt.DataTema;
+import org.eclipse.persistence.exceptions.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Urbina
+ */
+@WebServlet(name = "regiServlet", urlPatterns = {"/regiServlet"})
+public class regiServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet regiServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet regiServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    
+    private String escapeJson(String str) {
+        if (str == null) {
+            return null;
+        }
+        return str.replace("\\", "\\\\") // Escapa el carácter de barra invertida
+                .replace("\"", "\\\"") // Escapa las comillas dobles
+                .replace("\b", "\\b") // Escapa la retroceso
+                .replace("\f", "\\f") // Escapa el avance de página
+                .replace("\n", "\\n") // Escapa la nueva línea
+                .replace("\r", "\\r") // Escapa el retorno de carro
+                .replace("\t", "\\t");    // Escapa la tabulación
+    }
+    
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("\n-----regiServlet GET-----");
+        
+       
+        System.out.println("\n-----regiServlet GET-----");
+
+    }
+
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("\n-----regiServlet POST-----");
+        
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        // Obtener la sesión
+        HttpSession session = request.getSession();
+
+        // Leer el nickname desde la sesión
+        String nickname = (String) session.getAttribute("nickname");
+        LocalDate hoy = LocalDate.now();
+        String body = request.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual);
+        System.out.println("Body:" + body);
+         String os = null;
+         String navegador = null;
+         
+         
+         
+        try {
+            JSONObject jsonObject = new JSONObject(body);
+            os = jsonObject.getString("os"); 
+            
+            navegador = jsonObject.getString("nave");
+            
+           
+
+            System.out.println("os: " + os + "/navegaDoor: " + navegador);
+            
+        } catch (JSONException e) {
+            out.println("{\"success\": false, \"error\": \"Error al procesar la solicitud.\"}");
+            return; // Salir si hay un error al procesar el JSON
+        }
+        System.out.println("\n-----Pasado la construccion del json-----");
+        ControladorSuscripcion controlSus = new ControladorSuscripcion();
+        
+        
+       
+            
+        ControladorCliente controlCli = new ControladorCliente();
+        DataCliente cliente = controlCli.agregarRegistro(nickname,os,navegador,hoy);
+           
+          out.println("{\"success\": true}");
+          out.flush();   
+
+    
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Falta pasta. -Larry Capasta";
+    }// </editor-fold>
+
+}
--- src/main/webapp/scripts/Login.js
+++ src/main/webapp/scripts/Login.js
@@ -30,6 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
                 alert(message);
                 if (message === "Visitante logeado exitosamente.") {
+                    regitroMomento();
                     setTimeout(() => location.reload(), 1000);
                 }
             })
@@ -44,3 +45,51 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+
+function regitroMomento(){
+    //que ta' usando
+               var os = "nose";
+               var nave = "quien sabe";
+               
+                if (navigator.userAgent.indexOf("Win") !== -1) os =  
+                "Windows"; 
+                if (navigator.userAgent.indexOf("Mac") !== -1) os =  
+                  "Macintosh"; 
+                if (navigator.userAgent.indexOf("Linux") !== -1) os =  
+                  "Linux"; 
+                if (navigator.userAgent.indexOf("Android") !== -1) os =  
+                  "Android"; 
+                if (navigator.userAgent.indexOf("like Mac") !== -1) os =  
+                  "iOS"; 
+               
+               if (navigator.userAgent.indexOf("Chrome") !== -1) nave =  
+                "Chromium"; 
+                if (navigator.userAgent.indexOf("Firefox") !== -1) nave =  
+                  "FireFox"; 
+    //fin que ta´usando
+    
+    fetch('http://localhost:8080/EspotifyWeb/regiServlet', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: JSON.stringify({os: os, nave: nave})
+            })
+                    .then(response => {
+                        console.log("Respuesta del servidor:", response); // Para depuración
+
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.json();
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                        alert("Error registrar el login.");
+                    });
+    
+                 
+    
+    
+}
