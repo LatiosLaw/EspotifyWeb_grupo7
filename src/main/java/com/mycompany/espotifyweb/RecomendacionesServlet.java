0f4987aa3dc47942a880e65b4e3431fde970cd54
Nicolás Vázquez <oncedoxxe@gmail.com>
2024-11-23 16:53:56 -0300

    boston

--- src/main/java/com/mycompany/espotifyweb/RecomendacionesServlet.java
+++ src/main/java/com/mycompany/espotifyweb/RecomendacionesServlet.java
@@ -14,23 +14,6 @@ import persistencia.DAO_RegistroTema;
 
 public class RecomendacionesServlet extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AgregarTemaAListaServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AgregarTemaAListaServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("\n-----Recomendaciones GET-----");
@@ -65,12 +48,5 @@ public class RecomendacionesServlet extends HttpServlet {
         }
         System.out.println("\n-----Recomendaciones GET-----");
     }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       System.out.println("\n-----Recomendaciones POST-----");
-
-        System.out.println("\n-----Recomendaciones POST-----");
-    }
-
+    
 }
